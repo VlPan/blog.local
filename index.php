c14633e07cf8f45080124456760e379ff91d4410
VlPan <VladimirPant.bel@gmail.com>
2017-04-20 17:06:59 +0300

    +Abstract class Model

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
 //ТОЧКА ВХОДА INDEX.PHP
-require __DIR__. '/autoload.php';
+require __DIR__ . '/autoload.php';
+echo 'Hello world!';
+$article = \App\Models\Article::findAll();
+$categories= \App\Models\Categories::findAll();
+$authors = \App\Models\Authors::findAll();
+var_dump($article);
+var_dump($categories);
+var_dump($authors);
+
+
 
